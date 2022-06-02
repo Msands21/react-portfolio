504b48a6a58b262e642de207e573048ecf139dd2
Mike Sands <mikesands94@gmail.com>
2022-06-01 23:00:18 -0400

    projects and technologies

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm excited to help you build full-stack JavaScript projects! Check out some of my projects below.
+      </SectionText>
+      <Button onClick = {() => window.location = 'https://mikesands94@gmail.com'}> Email Me </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,37 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+<Section id="tech">
+  <SectionDivider />
+  <br />
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>
+    I've worked with a variety of Front-End and Back-End technologies including: 
+  </SectionText>
+  <List>
+    <ListItem>
+      <DiReact size="3rem" />
+      <ListContainer>
+        <ListTitle>Front-End</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          Javascript, HTML, CSS, jQuery, Bootsrap,
+          React.js
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+    <ListItem>
+      <DiFirebase size="3rem" />
+      <ListContainer>
+        <ListTitle>Back-End</ListTitle>
+        <ListParagraph>
+          Experience with <br />
+          Node.js, MySQL, MongoDB, Express.js
+        </ListParagraph>
+      </ListContainer>
+    </ListItem>
+  </List>
+</Section>
 );
 
 export default Technologies;
